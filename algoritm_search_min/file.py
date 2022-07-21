c9cfd14291dda6cbf3304f8c2c5c9b5c7f93eb04
OxY623 <fuzzydoorproductions0@gmail.com>
2022-07-21 18:54:20 +0300

    Create file,py

---
+++ algoritm_search_min/file.py
@@ -0,0 +1,12 @@
+def findSmallest(arr):
+    smallest = arr[0]
+    smallest_index = 0
+    for i in range(1,len(arr)):
+        if arr[i] < smallest:
+            smallest = arr[i]
+            smallest_index = i
+    return smallest
+
+
+x=[3,2,1,3,33,0]
+v=findSmallest(x)
