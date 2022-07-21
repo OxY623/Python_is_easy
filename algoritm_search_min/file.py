34dce3ce40a9b51ceab14c8a5362df099678e3a6
OxY623 <fuzzydoorproductions0@gmail.com>
2022-07-21 19:08:23 +0300

    Update file.py

--- algoritm_search_min/file.py
+++ algoritm_search_min/file.py
@@ -1,12 +1,18 @@
-def findSmallest(arr):
-    smallest = arr[0]
-    smallest_index = 0
-    for i in range(1,len(arr)):
-        if arr[i] < smallest:
-            smallest = arr[i]
-            smallest_index = i
-    return smallest
+def find_Smallest(a r r): 
+  smallest = arr[0]
+  smallest_index = 0
+  for i in range(l, len(arr)): 
+    if arr[i] < smallest: 
+      smallest = arr[i] 
+      smallest_index = i 
+  return smallest_index 
 
 
-x=[3,2,1,3,33,0]
-v=findSmallest(x)
+def selectionSort(arr): 
+   newArr = [] 
+   for i in range(len(arr)): 
+    smallest = indSmallest(arr) · 
+    newArr.append(arr.pop(smallest)) 
+   return newArr 
+
+print (selectionSort([S, З, 6, 2, 10])) 
