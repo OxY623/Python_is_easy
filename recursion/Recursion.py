c33a66194e2157ca2d412a2fcb93dcb8740954a3
OxY623 <fuzzydoorproductions0@gmail.com>
2022-07-28 20:52:31 +0300

    Update

--- recursion/Recursion.py
+++ recursion/Recursion.py
@@ -5,4 +5,29 @@ def countDown(n):
         return
     else:
         countDown(n-1);
-countDown(5);
+
+def sum(list): 
+  if list == [] : 
+     return 0 
+  return list[0] + sum(list[1: ])
+
+def countIndex(list):
+    if list == []:
+        return 0
+    return 1+ countIndex(list[1:])
+
+def max(array):
+    if len(array)==2:
+        if array[0]>array[1]:
+            return array[0]
+        else:
+            return array[1]
+    sub_max=max(array[1:])
+    if array[0] > sub_max:
+        return array[0]
+    else:
+        return sub_max
+
+print(sum([]))
+print(countIndex([1,23,3]))
+print(max([1,23,3]))
